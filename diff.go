4996954d7176ffeac1d88fb44c2be2e1d334f400
Daniel J. Rollins <daniel@djrollins.com>
2022-02-06 16:44:38 +0000

    feat: change CmpDiffComparator to StringDiffComparator
    Diffing byte arrays produces complete unreadable output. Reading to
    strings at least gives us something a human can understand as output.

--- diff.go
+++ diff.go
@@ -7,20 +7,20 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-// CmpDiffComparator reads expected and actual into strings compares them using
+// StringDiffComparator reads expected and actual into strings compares them using
 // cmp.Diff from the go-cmp library. On failure the diff string is returned.
-func CmpDiffComparator(expected, actual io.Reader) (ok bool, msg string) {
-	actualBytes, err := io.ReadAll(actual)
+func StringDiffComparator(expected, actual io.Reader) (ok bool, msg string) {
+	actualString, err := readToString(actual)
 	if err != nil {
 		msg = fmt.Sprintf("failed to read actual io.Reader: %v", err.Error())
 		return
 	}
-	expectedBytes, err := io.ReadAll(expected)
+	expectedString, err := readToString(expected)
 	if err != nil {
 		msg = fmt.Sprintf("failed to read expected io.Reader: %v", err.Error())
 		return
 	}
-	msg = cmp.Diff(expectedBytes, actualBytes)
+	msg = cmp.Diff(expectedString, actualString)
 	ok = msg == ""
 	return
 }
